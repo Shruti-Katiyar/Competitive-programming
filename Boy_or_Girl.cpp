058126e7d85cbc965b143d6fa1fe14bc0bdeac2b
Shruti Katiyar <shrutikatiyar459@gmail.com>
2022-03-21 23:05:50 +0530

    Boy Girl

---
+++ Boy_or_Girl.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespaces std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);	
+#endif
+
+    int n;
+    cin>>n;
+    cout<<n;
+
+}
