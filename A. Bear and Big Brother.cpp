f670752df4c69216c92a2833a3217af1ebe2a886
Shruti Katiyar <shrutikatiyar459@gmail.com>
2022-04-27 20:28:30 +0530

    Bear and Big Brother

---
+++ A. Bear and Big Brother.cpp
@@ -0,0 +1,20 @@
+//A. Bear and Big Brother
+//https://codeforces.com/problemset/problem/791/A
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,cnt=0;
+	cin>>a>>b;
+
+	while(a<=b){
+		a=a*3;
+		b=b*2;
+		cnt++;
+	
+	}
+
+	cout<<cnt;
+	
+	
+	return 0;
+}
