b14cddcb84a5286e4e005ea4fa4994d22cf1bc9e
Shruti Katiyar <shrutikatiyar459@gmail.com>
2022-04-28 14:45:11 +0530

    Watermelon

---
+++ A. Watermelon.cpp
@@ -0,0 +1,15 @@
+//A. Watermelon
+//https://codeforces.com/problemset/problem/4/A?csrf_token=02b7ccf0832d2c4372c46a6a0256ea1f
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int w;
+	cin>>w;
+	if(w%2==0 and w>2)cout<<"YES"<<endl;
+	else
+	cout<<"NO"<<endl;
+	return 0;
+}
