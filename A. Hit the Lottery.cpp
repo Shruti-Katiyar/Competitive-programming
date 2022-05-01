e6c22692322f2f0436294c28b06c5ee83e9c9d49
Shruti Katiyar <shrutikatiyar459@gmail.com>
2022-05-01 10:35:14 +0530

    Hit the Lottery

---
+++ A. Hit the Lottery.cpp
@@ -0,0 +1,20 @@
+//A. Hit the Lottery
+//https://codeforces.com/problemset/problem/996/A
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int ans;
+	while(n%10==0){
+	    ans++;
+    	n/=10;
+}
+cout<<ans;
+	
+	
+	return 0;
+	
+}
+
