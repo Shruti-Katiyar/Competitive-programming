b5373303b7b94244caf75313bef37e52258abacc
Shruti Katiyar <shrutikatiyar459@gmail.com>
2022-05-02 17:32:07 +0530

    FOX AND SNAKE

---
+++ FOX AND SNAKE.cpp
@@ -0,0 +1,34 @@
+//A. Fox And Snake
+//https://codeforces.com/problemset/problem/510/A
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n,m;
+	cin>>n>>m;
+	for(int i=1;i<=n;i++){
+		if(i%4==2){ //....................#
+			for(int i=1;i<m;i++){
+				cout<<".";
+			}
+			cout<<"#";
+		}
+		else if(i%4==0){ //#.......................
+			cout<<"#";
+			for(int i=1;i<m;i++){
+				cout<<".";
+			}
+		}
+		else{
+			for(int i=1;i<=m;i++){ // #########
+				cout<<"#";
+			}
+		}
+		cout<<endl;
+		
+	
+	}
+	
+	return 0;
+}
+
