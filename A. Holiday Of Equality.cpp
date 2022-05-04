d2ca52a66efac3f1c27630d7278a032d675e0e98
Shruti Katiyar <shrutikatiyar459@gmail.com>
2022-05-04 23:10:31 +0530

    Holiday Of Equality

---
+++ A. Holiday Of Equality.cpp
@@ -0,0 +1,30 @@
+//A. Holiday Of Equality
+//https://codeforces.com/problemset/problem/758/A
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n,sum=0;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+	cin>>a[i];
+}
+	
+	sort(a,a+n);//0 1 2 3 4 
+
+	
+	for(int i=0;i<n-1;i++){
+        sum+=(a[n-1]-a[i]);
+		                                                                                     
+}
+cout<<sum;
+
+return 0;
+}
+
+
+	
+		
+
